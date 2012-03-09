a5f95cafbe46994e33e4cd971e802d0e8a830404
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-03-09 17:56:44 +0000

    Broken initial pass at dsv
    git-svn-id: http://dev.zenoss.org/svnint/ClientServices/ZenPacks/ZenPacks.zenoss.oVirt@55157 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/oVirt/dynamicview.py
+++ ZenPacks/zenoss/oVirt/dynamicview.py
@@ -26,24 +26,68 @@ class ExampleComponentRelatable(DeviceComponentRelatable):
 ### IRelationsProvider Adapters
 
 class DeviceRelationsProvider_ovirt_datacenter(BaseRelationsProvider):
-    adapts(ExampleDevice)
+    adapts(BaseDevice)
 
     def relations(self, type=TAG_ALL):
         """
-        ExampleDevices impact all of their ExampleComponents.
-        """
-        if type in (TAG_ALL, TAG_IMPACTS):
-            for exampleComponent in self._adapted.exampleComponents():
-                yield self.constructRelationTo(exampleComponent, TAG_IMPACTS)
-
-
-class ExampleComponentRelationsProvider(BaseRelationsProvider):
-    adapts(GenericComponent)
-
-    def relations(self, type=TAG_ALL):
-        """
-        ExampleComponents are impacted by their ExampleDevice.
+        oVirt manager relates to data centers
         """
         if type in (TAG_ALL, TAG_IMPACTED_BY):
-            yield self.constructRelationTo(
-                self._adapted.exampleDevice(), TAG_IMPACTED_BY)
+            for dc in self._adapted.getComponents(meta_type='datacenters'):
+                yield self.constructRelationTo(dc, TAG_IMPACTED_BY)
+
+
+class OVirtComponentRelationsProvider(BaseRelationsProvider):
+    adapts(GenericComponent)
+
+    impactedByChain = {
+        'vms': 'hosts',
+        'hosts': 'clusters',
+        'clusters': 'datacenters',
+    }
+
+    impactsChain = {
+        'datacenters': 'clusters',
+        'clusters': 'hosts',
+        'hosts': 'vms',
+    }
+
+    def relations(self, type=TAG_ALL):
+        """
+        Match all oVirt types
+        """
+        # FIXME: make zp-specific component types to make matching easier
+        # FIXME: define a generic way to relate components via XML
+        element = self._adapted.component_type
+        if element not in (
+            'vm', 'vmpool', 'storage_domain', 'network', 'host', 'cluster', 'domain',
+            'data_center',
+        ):
+            raise StopIteration
+
+        dev = self._adapted.host()
+        guid = self._adapted.attributes.get('guid')
+        if guid is None:
+            # Model is broken as all components must have a guid
+            raise StopIteration
+
+        # What is this element impacted by?
+        if type in (TAG_ALL, TAG_IMPACTED_BY):
+            nextElementType = self.impactedByChain.get(element)
+            try:
+                obj = dev.getRelatedComponents(guid, nextElementType)[0]
+                if obj:
+                    yield self.constructRelationTo(obj, TAG_IMPACTED_BY)
+            except IndexError:
+                pass
+
+        # What does this element impact?
+        elif type in (TAG_IMPACTS,):
+            nextElementType = self.impactsChain.get(element)
+            try:
+                obj = dev.getRelatedComponents(guid, nextElementType)[0]
+                if obj:
+                    yield self.constructRelationTo(obj, TAG_IMPACTS)
+            except IndexError:
+                pass
+
