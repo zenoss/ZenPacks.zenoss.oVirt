44b99f2cf2480b2bdf34e5e34451f473f418df53
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-03-12 18:01:31 +0000

    Prep work for router
    git-svn-id: http://dev.zenoss.org/svnint/ClientServices/ZenPacks/ZenPacks.zenoss.oVirt@55217 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/oVirt/browser/resources/js/oVirt.js
@@ -0,0 +1,123 @@
+/*
+  ###########################################################################
+  # Copyright (C) 2012, Zenoss Inc.
+  ###########################################################################
+*/
+
+(function () {
+
+var NS = Ext.namespace('Zenoss.zenpacks.oVirt');
+
+
+// addoVirtInfra is the action (w/popup dialog) to add to the adddevice
+// button on Infrastructure->Devices
+var addOVirtInfra = new Zenoss.Action({
+    text: _t('Add oVirt Infrastructure') + '...',
+    id: 'addovirt-item',
+    permissionContext: '/zport/dmd/Devices/oVirt',
+    permission: 'Manage DMD',
+    handler: function(btn, e){
+        var win = new Zenoss.dialog.CloseDialog({
+            width: 300,
+            title: _t('Add oVirt Infrastructure'),
+            items: [{
+                xtype: 'form',
+                buttonAlign: 'left',
+                monitorValid: true,
+                labelAlign: 'top',
+                footerStyle: 'padding-left: 0',
+                border: false,
+                items: [{
+                    xtype: 'textfield',
+                    name: 'id',
+                    fieldLabel: _t('Name or ID'),
+                    id: "add-ovirt-id",
+                    allowBlank: false
+                }, {
+                    xtype: 'textfield',
+                    name: 'host',
+                    fieldLabel: _t('Host'),
+                    id: "add-ovirt-host",
+                    allowBlank: false
+                }, {
+                    xtype: 'checkbox',
+                    name: 'useSsl',
+                    fieldLabel: _t('Use SSL'),
+                    id: 'add-ovirt-usessl',
+                    checked: true
+                }, {
+                    xtype: 'textfield',
+                    name: 'username',
+                    fieldLabel: _t('Username'),
+                    id: "add-ovirt-username",
+                    allowBlank: false
+                }, {
+                    xtype: 'textfield',
+                    name: 'password',
+                    inputType: 'password',
+                    fieldLabel: _t('Password'),
+                    id: "add-ovirt-password",
+                    allowBlank: false
+                }, {
+                    xtype: 'combo',
+                    width: 160,
+                    name: 'collector',
+                    fieldLabel: _t('Collector'),
+                    id: 'add-ovirt-collector',
+                    mode: 'local',
+                    store: new Ext.data.ArrayStore({
+                        data: Zenoss.env.COLLECTORS,
+                        fields: ['name']
+                    }),
+                    valueField: 'name',
+                    displayField: 'name',
+                    forceSelection: true,
+                    editable: false,
+                    allowBlank: false,
+                    triggerAction: 'all',
+                    selectOnFocus: true,
+                    listeners: {
+                        'afterrender': function(component) {
+                            var index = component.store.find('name', 'localhost');
+                            if (index >= 0) {
+                                component.setValue('localhost');
+                            }
+                        }
+                    }
+                }],
+                buttons: [{
+                    xtype: 'DialogButton',
+                    id: 'addsingledevice-submit',
+                    text: _t('Add'),
+                    formBind: true,
+                    handler: function(b) {
+                        var form = b.ownerCt.ownerCt.getForm();
+                        var opts = form.getFieldValues();
+
+                        Zenoss.remote.OVirtRouter.addOVirtInfrastructure(opts,
+                        function(response) {
+                            if (response.success) {
+                                Zenoss.message.success(_t('Add oVirt Infrastructure job submitted. <a href="/zport/dmd/JobManager/jobs/{0}/viewlog">View Job Log</a>'),
+                                    response.jobId);
+                            }
+                            else {
+                                Zenoss.message.error(_t('Error adding oVirt Infrastructure: {0}'),
+                                    response.msg);
+                            }
+                        });
+                    }
+                }, Zenoss.dialog.CANCEL]
+            }]
+        });
+        win.show();
+    }
+});
+
+// push the addOVirtInfra action to the adddevice button
+Ext.ns('Zenoss.extensions');
+Zenoss.extensions.adddevice = Zenoss.extensions.adddevice instanceof Array ?
+                              Zenoss.extensions.adddevice : [];
+Zenoss.extensions.adddevice.push(addOVirtInfra);
+
+
+}());
