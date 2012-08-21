240fad5f96078bfbd6c575c7a532a4ba8b0473f7
Chet Luther <cluther@zenoss.com>
2012-08-21 11:51:09 -0500

    Roll version to 1.1.0dev0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.oVirt"
-VERSION = "1.0.3"
+VERSION = "1.1.0dev0"
 AUTHOR = "Zenoss Labs <labs@zenoss.com>"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
